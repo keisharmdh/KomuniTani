1e9df0533e83d0bdd82a7a3a60f9cc569c519347
keisha <keisha_ramadhani@apps.ipb.ac.id>
2024-10-23 23:46:33 +0700

    desain page reset & login

--- app/src/main/java/com/example/komunitani/daftar.java
+++ app/src/main/java/com/example/komunitani/daftar.java
@@ -1,6 +1,10 @@
 package com.example.komunitani;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,22 @@ public class daftar extends AppCompatActivity {
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
 //            return insets;
 //        });
+        Button btnDaftar = findViewById(R.id.btn_daftar);
+        btnDaftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(daftar.this, dashboard.class);
+                startActivity(intent);
+            }
+        });
+
+        TextView clickableText_login= findViewById(R.id.text_click_login);
+        clickableText_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(daftar.this, login.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/komunitani/reset_password.java
+++ app/src/main/java/com/example/komunitani/reset_password.java
@@ -1,6 +1,9 @@
 package com.example.komunitani;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,13 @@ public class reset_password extends AppCompatActivity {
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
 //            return insets;
 //        });
+        Button btnUbahPw = findViewById(R.id.btn_reset_pw);
+        btnUbahPw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(reset_password.this, dashboard.class);
+                startActivity(intent);
+            }
+        });
     }
 }
