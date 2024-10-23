ea8fc4abb4e49fb7c1befebe411c471c3ac5ad99
keisha <keisha_ramadhani@apps.ipb.ac.id>
2024-10-23 21:13:58 +0700

    buat function button autentikasi

--- app/src/main/java/com/example/komunitani/autentikasi.java
+++ app/src/main/java/com/example/komunitani/autentikasi.java
@@ -1,6 +1,9 @@
 package com.example.komunitani;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,24 @@ public class autentikasi extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button btnDaftar = findViewById(R.id.btn_daftar);
+        btnDaftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(autentikasi.this, daftar.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btnLogin = findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(autentikasi.this, login.class);
+                startActivity(intent);
+            }
         });
     }
 }
