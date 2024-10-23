a5508e420ca09163b417d03840e96d5f0d4cdf4f
keisha <keisha_ramadhani@apps.ipb.ac.id>
2024-10-23 16:44:39 +0700

    Desain page daftar & reset pw

--- app/src/main/java/com/example/komunitani/autentikasi.java
+++ app/src/main/java/com/example/komunitani/autentikasi.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class auth extends AppCompatActivity {
+public class autentikasi extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_auth);
+        setContentView(R.layout.activity_autentikasi);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
