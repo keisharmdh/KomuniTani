abde3c2538ebbaa035909c06a83c654b9e80b3c5
keisha <keisha_ramadhani@apps.ipb.ac.id>
2024-10-23 19:58:19 +0700

    Revisi page daftar

--- app/src/main/java/com/example/komunitani/splashscreen.java
+++ app/src/main/java/com/example/komunitani/splashscreen.java
@@ -1,6 +1,8 @@
 package com.example.komunitani;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,13 @@ public class splashscreen extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(splashscreen.this, autentikasi.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3000);
     }
 }
