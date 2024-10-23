0c93f4e90185cf1cc1de879dc12f385f438840ee
keisha <keisha_ramadhani@apps.ipb.ac.id>
2024-10-23 23:08:15 +0700

    buat function button login

--- app/src/main/java/com/example/komunitani/login.java
+++ app/src/main/java/com/example/komunitani/login.java
@@ -1,6 +1,10 @@
 package com.example.komunitani;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +23,34 @@ public class login extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+        });
+
+        Button btnLogin = findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(login.this, dashboard.class);
+                startActivity(intent);
+            }
+        });
+
+        TextView clickableText_lupa_password= findViewById(R.id.lupa_pw);
+        clickableText_lupa_password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(login.this, reset_password.class);
+                startActivity(intent);
+            }
+        });
+
+        TextView clickableText_daftar = findViewById(R.id.text_click_daftar);
+        clickableText_daftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(login.this, daftar.class);
+                startActivity(intent);
+            }
         });
     }
 }
